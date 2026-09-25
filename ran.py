53037c248dda301124170d277cdc7416ec6ae6fb
Zino <aromefrank69@gmail.com>
2026-09-25 22:40:02 +0100

    Add system info option

--- ran.py
+++ ran.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 
 def banner():
@@ -18,6 +19,7 @@ def main():
         print("""
 [1] Say Hello
 [2] About
+[3] System Info
 [0] Exit
 """)
 
@@ -30,6 +32,13 @@ def main():
         elif choice == "2":
             print("\nMyTool - Version 1.0")
             print("My first Python/GitHub project.")
+            input("\nPress Enter to continue...")
+
+        elif choice == "3":
+            print("\nSystem Information")
+            print("System:", platform.system())
+            print("Machine:", platform.machine())
+            print("Python:", platform.python_version())
             input("\nPress Enter to continue...")
 
         elif choice == "0":
